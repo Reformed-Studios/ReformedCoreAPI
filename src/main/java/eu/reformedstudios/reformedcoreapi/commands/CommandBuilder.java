78eee4a171c65bdef54026bcc64a03d479fbb106
muffin <mufin@mufin.live>
2022-02-11 14:00:34 +0100

    Made comment clearer

--- src/main/java/eu/reformedstudios/reformedcoreapi/commands/CommandBuilder.java
+++ src/main/java/eu/reformedstudios/reformedcoreapi/commands/CommandBuilder.java
@@ -37,7 +37,9 @@ public class CommandBuilder {
 	}
 
 	/**
-	 * Sets the command permissions, automatically gets checked on execute..
+	 * Sets the command permissions, automatically gets checked on execute. If your command uses multiple permissions,
+	 * i.e. "command.self", "command.other", add both in here and check individually depending on what the permission
+	 * allows them to do. You do <stong>NOT</stong> need to send an error message.
 	 *
 	 * @param permissions The permissions you want to pass
 	 * @return The builder for chaining purposes
